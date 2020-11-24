14987c5e2c893cd6d4b20ed28d5f90bf5f8f71d6
Arda Karaman <ardanincimaili@gmail.com>
2020-11-24 10:06:48 +0300

    added prefabs

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    [SerializeField] private float thrust = 10f;
+    [SerializeField] private RigidBody rb;
+
+    private Vector2D position;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
