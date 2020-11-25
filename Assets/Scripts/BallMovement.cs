cc2e7708da8db0b2ea39648872471d4bbd99e854
Arda Karaman <ardanincimaili@gmail.com>
2020-11-25 16:07:40 +0300

    added probuilder dependency

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -6,14 +6,37 @@ public class BallMovement : MonoBehaviour
 {
 
     [SerializeField] private float thrust = 10f;
-    [SerializeField] private RigidBody rb;
+    [SerializeField] private Rigidbody rb;
 
-    private Vector2D position;
+    private Vector2 lastPos;
+    public float startingThrust = 10f;
 
+    void Start()
+    {
+        rb.velocity = transform.forward * startingThrust;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 delta = Vector2.zero;
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 current = Input.mousePosition;
+
+            if (lastPos == Vector2.zero)
+                lastPos = current;
+
+            delta = current - lastPos;
+            lastPos = current;
+
+            Vector3 force = new Vector3(delta.x, 0, delta.y) * thrust;
+            rb.AddForce(force);
+        }
+        else
+        {
+            lastPos = Vector2.zero;
+        }
     }
 }
---
+++ Assets/Scripts/followplayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+
+    public Transform playerLoc;
+    public Vector3 offset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerLoc.position + offset;
+    }
+}
